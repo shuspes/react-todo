fbae4c8d1d0731b4de92cccd0070864a0d4295eb
shuspes <shuspes@gmail.com>
2018-02-11 17:50:52 +0300

    added redux-thunk to project.

--- src/store.js
+++ src/store.js
@@ -1,5 +1,11 @@
-import {createStore} from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const middleware = applyMiddleware(thunkMiddleware);
+
+const store = createStore(
+  reducers,
+  compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
 export default store;
